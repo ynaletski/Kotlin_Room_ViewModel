5a4eeb11402b2154cbbcb5cb4d75c78cf443b61e
Yauhen <64901358+ynaletski@users.noreply.github.com>
2022-03-22 14:38:22 +0300

    domain

---
+++ app/src/main/java/com/example/mystartkotlin/domain/usecases/InsertEventUseCase.kt
@@ -0,0 +1,21 @@
+package com.example.mystartkotlin.domain.usecases
+/*
+import com.example.mystartkotlin.data.repository.EventRepository
+import com.example.mystartkotlin.domain.model.Event
+
+class InsertEventUseCase constructor(
+    private val eventRepository: EventRepository
+    ): UseCases<Event,Int>() {
+    override suspend fun insert(event: Event) {
+        eventRepository.insert(event)
+    }
+
+    override suspend fun deleteAll() {
+        eventRepository.deleteAll()
+    }
+
+    override suspend fun deleteByEventId(id: Int) {
+        eventRepository.deleteByEventId(id)
+    }
+
+}*/
---
+++ app/src/main/java/com/example/mystartkotlin/domain/usecases/UseCases.kt
@@ -0,0 +1,7 @@
+package com.example.mystartkotlin.domain.usecases
+/*
+abstract class UseCases<E,ID> {
+    abstract suspend fun insert(event: E)
+    abstract suspend fun deleteAll()
+    abstract suspend fun deleteByEventId(id: ID)
+}*/
